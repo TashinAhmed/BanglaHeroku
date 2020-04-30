a656aea2ef8b9a30c20629f9c2ecfe4ce414c429
Tashin Ahmed <tahmed153109@bscse.uiu.ac.bd>
2020-05-01 04:15:52 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ app.config['SESSION_COOKIE_SECURE'] = False
 
 @app.route('/')
 def home():
-	return render_template("index.html", prediction_text="")
+	return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['post'])
 def predict():
     '''For rendering results on HTML GUI'''
-    inputs = request.form.values() 
+    inputs = request.form.values()
     input_lists = webtool.convert(inputs)
-    	if input_lists != 0:
-		for i in input_lists:
-		    webtool.MakeError(i)
-		output = webtool.listToString(webtool.nlist)
-	else:
-		ouput = ""
+    if input_lists != 0:
+        for i in input_lists:
+            webtool.MakeError(i)
+        output = webtool.listToString(webtool.nlist)
+    else:
+        ouput = ""
     
     return render_template("index.html", prediction_text=output)
 if __name__ == "__main__":
