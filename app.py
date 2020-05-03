f6ec2b90afcbd0eb2794a496d8352c4dd13959dd
Tashin Ahmed <tahmed153109@bscse.uiu.ac.bd>
2020-05-03 15:22:33 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -15,15 +15,17 @@ def predict():
     '''For rendering results on HTML GUI'''
     inputs = request.form.values()
     input_lists = webtool.convert(inputs)
-    output = []
+    inp_lists = ""
+    output = ""
     if len(input_lists) != 0:
         for i in input_lists:
-            output.append(webtool.fun(i))
+            output += webtool.fun(i)
+            inp_lists += i + " "
         # inputs = 0
         input_lists.clear()
     else:
         return render_template("index.html",prediction_text = len(input_lists))
     
-    return render_template("index.html", prediction_text = output)
+    return render_template("index.html",input_text = "input: " + inp_lists, prediction_text = "output: " + output)
 if __name__ == "__main__":
     app.run(debug=True)
